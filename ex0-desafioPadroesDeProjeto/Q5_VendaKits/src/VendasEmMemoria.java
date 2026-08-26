fcb8dbe21bad1f3fa157515c925a24f2829f5983
Dajosonh <davibandeiradelima@gmail.com>
2026-08-26 19:25:22 -0300

    Ajeitando Q5

--- ex0-desafioPadroesDeProjeto/Q5_VendaKits/src/VendasEmMemoria.java
+++ ex0-desafioPadroesDeProjeto/Q5_VendaKits/src/VendasEmMemoria.java
@@ -3,35 +3,49 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class VendasEmMemoria implements VendasFachada{
-	private List<Produto> produtos;
-	
-	public VendasEmMemoria() {
-		produtos = new ArrayList<>();
-		produtos.add(new Produto(1, "Caneta", 1.55));
-		produtos.add(new Produto(2, "Borracha", 1.15));
-		produtos.add(new Produto(3, "Caderno", 32.99));
-	}
-	
-	@Override
-	public Venda iniciarVenda() {
-		return new Venda(LocalDateTime.now());
-	}
+public class VendasEmMemoria implements VendasFachada {
+    private List<Oferta> ofertas;
 
-	@Override
-	public void registrarVenda(Venda umaVenda, int codigoProduto, int quantidade) {
-		Produto prod = produtos.stream().filter(p -> p.getId() == codigoProduto).findFirst().get();
-		umaVenda.registrarVenda(prod, quantidade);
-	}
+    public VendasEmMemoria() {
+        ofertas = new ArrayList<>();
 
-	@Override
-	public String emitirComprovante(Venda umaVenda) {
-		return umaVenda.emitirComprovante();
-	}
+        Produto caneta = new Produto(1, "Caneta", 1.55);
+        Produto borracha = new Produto(2, "Borracha", 1.15);
+        Produto caderno = new Produto(3, "Caderno", 32.99);
 
-	@Override
-	public List<Produto> buscarProdutos() {
-		return Collections.unmodifiableList(produtos);
-	}
+        Kit kitEscolar = new Kit(4, "Kit Escolar");
+        kitEscolar.adicionar(caneta);
+        kitEscolar.adicionar(borracha);
+        kitEscolar.adicionar(caderno);
+
+        ofertas.add(caneta);
+        ofertas.add(borracha);
+        ofertas.add(caderno);
+        ofertas.add(kitEscolar);
+    }
+
+    @Override
+    public Venda iniciarVenda() {
+        return new Venda(LocalDateTime.now());
+    }
+
+    @Override
+    public void registrarVenda(Venda umaVenda, int codigoProduto, int quantidade) {
+        Oferta oferta = ofertas.stream()
+                .filter(o -> o.getId() == codigoProduto)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Oferta nao encontrada"));
+        umaVenda.registrarVenda(oferta, quantidade);
+    }
+
+    @Override
+    public String emitirComprovante(Venda umaVenda) {
+        return umaVenda.emitirComprovante();
+    }
+
+    @Override
+    public List<Oferta> buscarProdutos() {
+        return Collections.unmodifiableList(ofertas);
+    }
 
 }
