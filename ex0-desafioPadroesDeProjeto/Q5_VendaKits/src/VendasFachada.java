d10e36137ced09993e12b7c11d269686e13f92eb
Dajosonh <davibandeiradelima@gmail.com>
2026-08-26 19:26:58 -0300

    Ajeitando Q5

--- ex0-desafioPadroesDeProjeto/Q5_VendaKits/src/VendasFachada.java
+++ ex0-desafioPadroesDeProjeto/Q5_VendaKits/src/VendasFachada.java
@@ -8,18 +8,19 @@ public interface VendasFachada {
      * @return a venda recém criada
      */
     Venda iniciarVenda();
-    
+
     /**
      * Registra um novo item de venda de produto em uma venda
      *
-     * @param umaVenda a venda atual (aina não fechada) sendo processada
+     * @param umaVenda      a venda atual (aina não fechada) sendo processada
      * @param codigoProduto o código do produto vendido
-     * @param quantidade a quantidade vendida do produto
+     * @param quantidade    a quantidade vendida do produto
      */
     void registrarVenda(Venda umaVenda, int codigoProduto, int quantidade);
-    
+
     /**
      * Emite o comprovante de venda a ser impresso
+     *
      * @param umaVenda a venda para emissão do comprovante
      * @return uma String representando o comprovante de venda ser impresso
      */
@@ -27,7 +28,9 @@ public interface VendasFachada {
 
     /**
      * Consulta a lista de produtos disponíveis para venda
+     *
      * @return a lista de produtos disponíveis para venda
      */
-    List<Produto> buscarProdutos();
+    List<Oferta> buscarProdutos();
+
 }
