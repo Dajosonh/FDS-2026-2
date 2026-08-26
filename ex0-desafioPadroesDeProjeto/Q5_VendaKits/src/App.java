4cf1a361c24b76729010168c4858789250c9f83a
Dajosonh <davibandeiradelima@gmail.com>
2026-08-26 19:14:51 -0300

    Ajeitando Q5

--- ex0-desafioPadroesDeProjeto/Q5_VendaKits/src/App.java
+++ ex0-desafioPadroesDeProjeto/Q5_VendaKits/src/App.java
@@ -1,12 +1,14 @@
 import java.util.List;
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         VendasFachada proc = new VendasEmMemoria();
-        List<Produto> produtos = proc.buscarProdutos();
+        List<Oferta> ofertas = proc.buscarProdutos();
         Venda venda = proc.iniciarVenda();
-        proc.registrarVenda(venda, produtos.get(0).getId(), 1);
-        proc.registrarVenda(venda, produtos.get(1).getId(), 2);
+
+        proc.registrarVenda(venda, ofertas.get(0).getId(), 2);
+        proc.registrarVenda(venda, ofertas.get(3).getId(), 1);
+
         System.out.println(proc.emitirComprovante(venda));
     }
 }
