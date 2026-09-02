56c544a5433ebeaf13ec10c050e4e43c153f0ffa
Gabi Zanini <gabizaninisilva@gmail.com>
2026-09-02 21:58:16 +0000

    ex1-changes2

--- ex1-biblioteca-v1-basico/ex1-biblioteca-v1-basico/src/main/java/com/bcopstein/ex1biblioeca/Controller.java
+++ ex1-biblioteca-v1-basico/ex1-biblioteca-v1-basico/src/main/java/com/bcopstein/ex1biblioeca/Controller.java
@@ -1,11 +1,24 @@
 package com.bcopstein.ex1biblioeca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    private final List<Livro> livros;
+
+    public Controller() {
+        livros = new ArrayList<>();
+        livros.add(new Livro(1, "A Metamorfose", "Franz Kafka", 1915));
+        livros.add(new Livro(2, "Declínio de um Homem", "Osamu Dazai", 1948));
+        livros.add(new Livro(3, "O Estrangeiro", "Albert Camus", 1942));
+    }
+
     @GetMapping("")
     @CrossOrigin(origins = "*")
     public String mensagemDeBemVindo() {
@@ -14,7 +27,7 @@ public class Controller {
 
     @GetMapping("/livros")
     @CrossOrigin(origins = "*")
-    public String listaLivros() {
-        return "Esta e a lista de livros";
+    public List<Livro> listaLivros() {
+        return livros;
     }
 }
